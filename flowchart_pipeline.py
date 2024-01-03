616e063b57e65f40ffdbda0e7c59a4efbe45241d
racarvajal <rcarvajalp@outlook.com>
2024-01-03 22:59:52 +0000

    create extended flowchart

---
+++ flowchart_pipeline.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import schemdraw
+from schemdraw import flow
+import schemdraw.elements as elm
+import global_variables as gv
+# import matplotlib as mpl
+# import matplotlib.pyplot as plt
+
+# mpl.rcdefaults()
+# plt.rcParams['text.usetex'] = True
+
+with schemdraw.Drawing(show=False) as d:
+    init           = flow.Start(h=1.5, w=2.75).at((0, 0)).label('SOURCE\nFROM\nCATALOGUE')
+    AGN_gal_model  = flow.Decision(h=2.25, w=3.75, W='Predicted\nas AGN', E='Predicted\nas Galaxy').at((0, -2.5)).label('AGN\nCLASSIFICATION\nMODEL')
+    elm.Wire('-', arrow='->').at(init.S).to(AGN_gal_model.N)
+    rAGN_model     = flow.Decision(h=2.25, w=4, S='Predicted\nas radio', E='Predicted\nas no radio').at((-5, -4.5)).label('AGN RADIO\nDETECTION\nMODEL')
+    rGal_model     = flow.Decision(h=2.25, w=4, S='Predicted\nas radio', W='Predicted\nas no radio').at((5, -4.5)).label('GAL. RADIO\nDETECTION\nMODEL')
+    elm.Wire('-|', arrow='->').at(AGN_gal_model.W).to(rAGN_model.N)
+    elm.Wire('-|', arrow='->').at(AGN_gal_model.E).to(rGal_model.N)
+    discarded      = flow.StateEnd(r=1.25).at((0, -6.5)).label('DISCARDED\nSOURCE')
+    z_rAGN_model   = flow.Box(h=1.5, w=2.5).at((-5, -7.75)).label('RAGN Z\nPREDICTION\nMODEL')
+    z_rGal_model   = flow.Box(h=1.5, w=2.5).at((5, -7.75)).label('RGAL Z\nPREDICTION\nMODEL')
+    elm.Wire('-', arrow='->').at(rAGN_model.S).to(z_rAGN_model.N)
+    elm.Wire('-', arrow='->').at(rGal_model.S).to(z_rGal_model.N)
+    elm.Wire('-|', arrow='->').at(rAGN_model.E).to(discarded.N)
+    elm.Wire('-|', arrow='->').at(rGal_model.W).to(discarded.N)
+    final_rAGN     = flow.Box(h=1.5, w=2.5).at((-5, -10.25)).label('PREDICTED\nRADIO AGN\nW/REDSHIFT')
+    final_rGal     = flow.Box(h=1.5, w=2.5).at((5, -10.25)).label('PREDICTED\nRADIO GAL\nW/REDSHIFT')
+    elm.Wire('-', arrow='->').at(z_rAGN_model.S).to(final_rAGN.N)
+    elm.Wire('-', arrow='->').at(z_rGal_model.S).to(final_rGal.N)
+    final_compile  = flow.Box(h=1.5, w=2.5).at((0, -11.75)).label('COMPILE\nPREDICTIONS')
+    elm.Wire('|-', arrow='->').at(final_rAGN.S).to(final_compile.W)
+    elm.Wire('|-', arrow='->').at(final_rGal.S).to(final_compile.E)
+    final_state    = flow.StateEnd(r=1.25).at((1.25, -15.25)).label('FINAL\nPREDICTED\nSOURCES')
+    elm.Wire('-', arrow='->').at(final_compile.S).to(final_state.N)
+
+    final_ghost    = flow.Start(h=1.0, w=2.5).at((0, -18.0))
+    # d.draw(show=True)
+    # d.save(gv.plots_path + 'flowchart_pipeline_extended.pdf')
+print('EOF')
