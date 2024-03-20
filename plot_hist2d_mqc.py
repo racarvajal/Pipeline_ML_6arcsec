3ebfdabcf87d1be287e3e454aedc50e885bd9565
racarvajal <rcarvajalp@outlook.com>
2024-03-20 22:32:34 +0000

    use skyproj 1.2.2

--- plot_hist2d_mqc.py
+++ plot_hist2d_mqc.py
@@ -4,25 +4,15 @@
 # of sources in the
 # Million Quasar Catalog (MQC)
 
-from os import path, rename
 import numpy as np
 import matplotlib as mpl
-import matplotlib.cm as cm
-from matplotlib import ticker
 import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
 import matplotlib.patheffects as mpe
-import matplotlib.patches as mpatches
-from matplotlib.ticker import ScalarFormatter
-from astropy.visualization import LogStretch, PowerStretch
-from astropy.visualization.mpl_normalize import ImageNormalize
-from astropy.coordinates import SkyCoord
-from astropy import units as u
 from astropy.io import fits
 import colorcet as cc
 import cmasher as cmr
 import skyproj
-import global_variables as gv
 
 # Path effects for labels and plots.
 pe1            = [mpe.Stroke(linewidth=5.0, foreground='black'),
@@ -37,8 +27,7 @@ mpl.rcdefaults()
 plt.rcParams['text.usetex'] = True
 plt.rcParams['xtick.labelsize'] = 16
 plt.rcParams['ytick.labelsize'] = 16
-plt.rcParams['path.effects'] = pe2
-
+plt.rcParams['path.effects']    = pe2
 
 # temporal path for mqc data (mac)
 mqc_version = '7_4d' # '8', '7_4d', '7_4c', '7_6', '7_7', '7_8d', etc. '7_4d' used
@@ -57,15 +46,12 @@ ax1  = fig.add_subplot(111)
 sp = skyproj.HammerSkyproj(ax=ax1, rcparams=ax_props, lon_0=0)
 # sp = skyproj.McBrydeSkyproj(ax=ax1, rcparams=ax_props, lon_0=0)
 
-norm_sources  = ImageNormalize(stretch=PowerStretch(0.30))
-sp.draw_hpxbin(data_ra, data_dec, cmap='cet_bmy', nside=128, xsize=400)
-# sp.draw_colorbar(pad=0.05,
-#                 label=r'$\mathrm{Number ~ of ~ sources ~ per ~ bin}$',
-#                 fontsize=16, fraction=0.025)
+norm_sources  = mcolors.LogNorm(vmin=1, vmax=115)
+sp.draw_hpxbin(data_ra, data_dec, cmap='cet_bmy', nside=128, xsize=400, norm=norm_sources)
 sp.draw_colorbar(pad=0.01, label=r'$\mathrm{Number ~ of ~ sources ~ per ~ bin}$',
-                fontsize=16, fraction=0.025)
-sp.set_xlabel(r'$\mathrm{Right ~ Ascension} ~ (\mathrm{J}2000)$')
-sp.set_ylabel(r'$\mathrm{Declination} ~ (\mathrm{J}2000)$')
+                fontsize=16, fraction=0.025, extend='max')
+sp.ax.set_xlabel(r'$\mathrm{Right ~ Ascension} ~ (\mathrm{J}2000)$')
+sp.ax.set_ylabel(r'$\mathrm{Declination} ~ (\mathrm{J}2000)$')
 sp.ax.tick_params(axis='both', which='major', labelsize=30)
 # plt.tight_layout()
 # plt.savefig(gv.plots_path + f'MQC_v{mqc_version}_source_density_map.pdf', bbox_inches='tight')
